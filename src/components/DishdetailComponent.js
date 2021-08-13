62d4a84c1385843995d4096f7926fabe84955049
keen-ToLearn <shantanujoshi200@gmail.com>
2021-08-14 00:38:53 +0530

    Class to Functional component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,74 +1,136 @@
-import React, {Component} from 'react';
+import React from 'react';
+//import React, {Component} from 'react';
 import { Card, CardImg, CardTitle, CardBody, CardText } from 'reactstrap';
 import {Media} from 'reactstrap';
 
-class DishDetail extends Component{
-    constructor(props){
-        super(props);
-
-        // this.state = {
-        //     dishinfo: this.props.selectedDish
-        // }
-        //commenting out state as props can be directly used
-        //no state makes this component stateless and presentational
-        console.log(this.props.selectedDish);
-    }
-    renderDish(info){
-        if(info != null){
-            return(
-                <Card>
-                    <CardImg width="100%" src={info.image} alt={info.name}/>
-                    <CardBody>
-                        <CardTitle>{info.name}</CardTitle>
-                        <CardText>{info.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else
-            return(<div></div>);
-    }
-
-    renderComments(info){
-        if(info != null){
-            const review = info.comments.map((comment) => {
-                return(
-                    <div key={comment.id}>
-                        <Media tag="li">
-                            <Media body>
-                                <p>{comment.comment}</p>
-                                <p>--{comment.author}, {comment.date.slice(0,10)}</p>
-                            </Media>
-                        </Media>
-                    </div>
-                );
-            });
-            return(
-                <Media list className="list-unstyled">
-                    <h4>Comments</h4>
-                    {review}
-                </Media>
-            );
-        }
-        else
-            return(<div></div>);
-    }
-
-    render(){
-        console.log(this.props.selectedDish);
+function RenderDish({info}){
+    if(info != null){
         return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.selectedDish)}
-                    </div>
-                    <div className="col-md-5 col-12 m-1">
-                            {this.renderComments(this.props.selectedDish)}
-                    </div>
-                </div>
-            </div>
+            <Card>
+                <CardImg width="100%" src={info.image} alt={info.name}/>
+                <CardBody>
+                    <CardTitle>{info.name}</CardTitle>
+                    <CardText>{info.description}</CardText>
+                </CardBody>
+            </Card>
         );
     }
+    else
+        return(<div></div>);
 }
+
+function RenderComments({info}){
+    if(info != null){
+        const review = info.comments.map((comment) => {
+            return(
+                <div key={comment.id}>
+                    <Media tag="li">
+                        <Media body>
+                            <p>{comment.comment}</p>
+                            <p>--{comment.author}, {comment.date.slice(0,10)}</p>
+                        </Media>
+                    </Media>
+                </div>
+            );
+        });
+        return(
+            <Media list className="list-unstyled">
+                <h4>Comments</h4>
+                {review}
+            </Media>
+        );
+    }
+    else
+        return(<div></div>);
+}
+
+const DishDetail = (props) => {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish info={props.selectedDish}/>
+                </div>
+                <div className="col-md-5 col-12 m-1">
+                    <RenderComments info={props.selectedDish}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+// class DishDetail extends Component{
+//     // constructor(props){
+//     //     super(props);
+
+//     //     // this.state = {
+//     //     //     dishinfo: this.props.selectedDish
+//     //     // }
+//     //     //commenting out state as props can be directly used
+//     //     //no state makes this component stateless and presentational
+//     //     console.log('DishDetail component constructor invoked');
+//     // }
+
+//     componentDidUpdate(){
+//         console.log('DishDetail component componentDidUpdate invoked');
+//     }
+
+//     renderDish(info){
+//         if(info != null){
+//             return(
+//                 {/*<Card>
+//                     <CardImg width="100%" src={info.image} alt={info.name}/>
+//                     <CardBody>
+//                         <CardTitle>{info.name}</CardTitle>
+//                         <CardText>{info.description}</CardText>
+//                     </CardBody>
+//                 </Card>*/}
+//             );
+//         }
+//         else
+//             return({/*<div></div>*/});
+//     }
+
+//     renderComments(info){
+//         if(info != null){
+//             const review = info.comments.map((comment) => {
+//                 return(
+//                     {/*<div key={comment.id}>
+//                         <Media tag="li">
+//                             <Media body>
+//                                 <p>{comment.comment}</p>
+//                                 <p>--{comment.author}, {comment.date.slice(0,10)}</p>
+//                             </Media>
+//                         </Media>
+//                     </div>*/}
+//                 );
+//             });
+//             return(
+//                 {/*<Media list className="list-unstyled">
+//                     <h4>Comments</h4>
+//                     {review}
+//                 </Media>*/}
+//             );
+//         }
+//         else
+//             return({/*<div></div>*/});
+//     }
+
+//     render(){
+//         console.log(this.props.selectedDish);
+//         return(
+//             {/*<div className="container">
+//                 <div className="row">
+//                     <div className="col-12 col-md-5 m-1">
+//                         {this.renderDish(this.props.selectedDish)}
+//                     </div>
+//                     <div className="col-md-5 col-12 m-1">
+//                             {this.renderComments(this.props.selectedDish)}
+//                     </div>
+//                 </div>
+//             </div>*/}
+//         );
+//     }
+// }
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,108 +1,125 @@
-import React, { Component } from 'react';
-//import { Media } from 'reactstrap';
+//import React, { Component } from 'react';
+import React from 'react';
 import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 //import {CardBody, CardText} from 'reactstrap';
-//'CARD' REACTSTRAP COMPONENT WOULD NOW BE USED TO DETERMINE THE LAYOUT INSTEAD OF THE 'MEDIA' REACTSTRAP COMPONENT
 
-//creating a class component
-class Menu extends Component{
-    //constructor which conventionally takes props as argument
-    constructor(props){
-        //invoking constructor of super class with 'props' parameter
-        super(props);
-        //adding data into component for use
-        //defining a state for 'Menu' component
-        //state stores properties related to component which may be used
-        console.log('Menu component constructor invoked');
-    }
+//creating a functional component which is presentational in nature
+//Method 1 of passing props - directly specifying the objects passed through props
+function RenderMenuItem({dish, onClick}){
+    return(
+        <Card onClick={() => onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name}/>
+            <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
 
-    componentDidMount(){
-        console.log('Menu component componentDidMount invoked');
-    }
-
-//to render the dish that was selected
-
-//    renderDish(dish){
-//        if(dish != null){
-//            return(
-//                {/*
-//                <Card>
-//                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
-//                    <CardBody>
-//                        <CardTitle>{dish.name}</CardTitle>
-//                        <CardText>{dish.description}</CardText>
-//                    </CardBody>
-//                </Card>
-//                */}
-//            );
-//        }
-//        else{
-//            return({/*<div></div>*/});
-//        }
-//    }
-
-//render method that must be implemented for every component
-    render(){
-        //initialising 'menu'
-        //map over all dish in dishes property of state
-        //arrow function: take a dish and return following
-        const menu = this.props.dishes.map((dish) => {
-        /*
-            const menu = this.state.dishes.map((dish) => {...}
-            above line now needs to be changed as only props are being used
-            change 'state' to 'props'
-        */
-            //returning a layout for every dish
-            return(
-                //key helps identify different list objects, dish.id has been used as a key
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    {/*<comment>Clicking on card should load information about dish</comment>*/}
-                    <Card onClick={() => this.props.onClick(dish.id)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                    {/*
-                    <comment>A media list item 'li'</comment>
-                    <Media tag="li">
-                        <Media left middle>
-                            */}
-                            {/*<comment>Media object to add image</comment>*/}
-                            {/*
-                            <Media object src={dish.image} alt={dish.name}/>
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>*/}
-                            {/*<Media>{dish.description}</Media>*/}
-                            {/*
-                            <p>{dish.description}</p>
-                        </Media>
-                    </Media>
-                    */}
-                </div>
-            );
-        });
-        console.log('Menu component render invoked');
+//Method 2 of passing props
+//using arrow function to implement 'Menu' functional component
+const Menu = (props) => {
+    const menu = props.dishes.map((dish) => {
         return(
-            <div className="container">
-                <div className="row">
-                    {/*<Media list>*/}
-                        {menu}
-                    {/*</Media>*/}
-                </div>
-                {/*
-                <div className="row">
-                    <div className="col-12 m-1">
-                        {this.renderDish(this.state.selectedDish)}
-                    </div>
-                </div>
-                <comment>renderDish method needs to be implemented differently for assignment 1</comment>
-                */}
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RenderMenuItem dish={dish} onClick={props.onClick}/>
             </div>
         );
-    }
+    });
+
+    return(
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    );
 }
+
+//creating a class component
+// class Menu extends Component{
+//     //constructor which conventionally takes props as argument
+//     //below constructor is 'useless as it is not maintaining any state or performing any other function
+//     // constructor(props){
+//     //     //invoking constructor of super class with 'props' parameter
+//     //     super(props);
+//     //     //adding data into component for use
+//     //     //defining a state for 'Menu' component
+//     //     //state stores properties related to component which may be used
+//     //     console.log('Menu component constructor invoked');
+//     // }
+
+//     componentDidMount(){
+//         console.log('Menu component componentDidMount invoked');
+//     }
+
+// //to render the dish that was selected
+
+// //    renderDish(dish){
+// //        if(dish != null){
+// //            return(
+// //                {/*
+// //                <Card>
+// //                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+// //                    <CardBody>
+// //                        <CardTitle>{dish.name}</CardTitle>
+// //                        <CardText>{dish.description}</CardText>
+// //                    </CardBody>
+// //                </Card>
+// //                */}
+// //            );
+// //        }
+// //        else{
+// //            return({/*<div></div>*/});
+// //        }
+// //    }
+
+//     renderDish(dish){
+//         if(dish != null)
+//             return(
+//                 {/*<DishDetail selectedDish={dish}/>*/}
+//             );
+//         else
+//             return({/*<div></div>*/});
+//     }
+// //render method that must be implemented for every component
+//     render(){
+//         //initialising 'menu'
+//         //map over all dish in dishes property of state
+//         //arrow function: take a dish and return following
+//         const menu = this.props.dishes.map((dish) => {
+//             //returning a layout for every dish
+//             return(
+//                 //key helps identify different list objects, dish.id has been used as a key
+//                 {/*<div key={dish.id} className="col-12 col-md-5 m-1">
+//                     <comment>Clicking on card should load information about dish</comment>
+//                     <Card onClick={() => this.props.onClick(dish.id)}>
+//                         <CardImg width="100%" src={dish.image} alt={dish.name}/>
+//                         <CardImgOverlay>
+//                             <CardTitle>{dish.name}</CardTitle>
+//                         </CardImgOverlay>
+//                     </Card>
+//                 </div>*/}
+//             );
+//         });
+//         console.log('Menu component render invoked');
+//         return(
+//             {/*<div className="container">
+//                 <div className="row">
+//                     {menu}
+//                 </div>
+//                 <div className="row">
+//                     <div className="col-12 m-1">
+//                         {this.renderDish(this.state.selectedDish)}
+//                     </div>
+//                 </div>
+//                 <comment>renderDish method needs to be implemented differently for assignment 1</comment>
+//                 {this.renderDish(this.state.selectedDish)}
+//             </div>*/}
+//         );
+//     }
+// }
+
 //export the component from here
 //so that it can be imported eslewhere
 export default Menu;
