4680849df1f98ba7cdf5e441056ce2ef9d4b068e
keen-ToLearn <shantanujoshi200@gmail.com>
2021-08-21 22:14:53 +0530

    Converted app to SPA

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 //import { Navbar, NavbarBrand } from 'reactstrap';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 //import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 //Making main component as container component
@@ -16,13 +20,20 @@ class Main extends Component{
         //bringing the states from Menu component here
         this.state = {
             dishes : DISHES,
+            comments : COMMENTS,
+            promotions : PROMOTIONS,
+            leaders : LEADERS
         };
     }
 
     render(){
         const HomePage = () => {
             return(
-                <Home/>
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
 
@@ -38,6 +49,7 @@ class Main extends Component{
                         To pass props inline functional component or function has to be used, as per Route specification
                     </comment>*/}
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+                    <Route exact path="/contactus" component={Contact}/>
                     <Redirect to="/home"/>
                 </Switch>
                 <Footer/>
