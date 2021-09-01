597e15eeebdf8575c01ec54f01fb0377b18be0e8
keen-ToLearn <shantanujoshi200@gmail.com>
2021-09-02 01:27:41 +0530

    Added Controlled Form to ContactComponent

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,50 +1,177 @@
-import React from 'react';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, {Component} from 'react';
+import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
 import {Link} from 'react-router-dom';
 
-function Contact(props) {
-    return(
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem>
-                        <Link to="/home">Home</Link>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Contact Us</h3>
-                    <hr/>
+//Contact component has been converted into a class component from a functional component
+class Contact extends Component{
+    constructor(props){
+        super(props);
+        //the state is being maintained for syncing with controlled form
+        this.state = {
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: '',
+            agree: false,
+            contactType: 'Tel.',
+            message: ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    //'event' is associated with Form elements like Input or Button
+    //function to handle changes to input field and update the component states simulataneously
+    handleInputChange(event){
+        //event.target - details of Input field in Form that has changed
+        const target = event.target;
+        //target refers to an Input field hence, target has a type
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    //function to submit the form data
+    handleSubmit(event){
+        console.log("Current state is: " + JSON.stringify(this.state));
+        alert("Current state is: " + JSON.stringify(this.state));
+        //To prevent the default behaviour of opening a new browser window; 'event.preventDefault()' method is used
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/home">Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Contact Us</h3>
+                        <hr/>
+                    </div>
                 </div>
-            </div>
-            <div className="row row-content">
-                <div className="col-12">
-                <h3>Location Information</h3>
+                <div className="row row-content">
+                    <div className="col-12">
+                    <h3>Location Information</h3>
+                    </div>
+                    <div className="col-12 col-sm-4 offset-sm-1">
+                        <h5>Our Address</h5>
+                        <address>
+                        121, Clear Water Bay Road<br />
+                        Clear Water Bay, Kowloon<br />
+                        HONG KONG<br />
+                        <i className="fa fa-phone"></i>: +852 1234 5678<br />
+                        <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                        <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                        </address>
+                    </div>
+                    <div className="col-12 col-sm-6 offset-sm-1">
+                        <h5>Map of our Location</h5>
+                    </div>
+                    <div className="col-12 col-sm-11 offset-sm-1">
+                        <div className="btn-group" role="group">
+                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-info" href="/home"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-12 col-sm-4 offset-sm-1">
-                    <h5>Our Address</h5>
-                    <address>
-                    121, Clear Water Bay Road<br />
-                    Clear Water Bay, Kowloon<br />
-                    HONG KONG<br />
-                    <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                    <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                    <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
-                    </address>
-                </div>
-                <div className="col-12 col-sm-6 offset-sm-1">
-                    <h5>Map of our Location</h5>
-                </div>
-                <div className="col-12 col-sm-11 offset-sm-1">
-                    <div className="btn-group" role="group">
-                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                        <a role="button" className="btn btn-info" href="/home"><i className="fa fa-skype"></i> Skype</a>
-                        <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                {/*<comment>Controlled form below</comment>*/}
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h3>Send us Your Feedback</h3>
+                    </div>
+                    <div className="col-12 col-md-9">
+                        <Form onSubmit={this.handleSubmit}>
+                            {/*
+                            <comment>A 'FormGroup row' refers to one row of a form</comment>
+                            <comment>'FormGroup row' implements bootstrap grid, FormGroup elements can have classes to specify layout</comment>
+                            */}
+                            <FormGroup row>
+                                <Label htmlFor="firstname" md={2}>First Name</Label>
+                                {/*
+                                    <div className="col-md-10"></div>
+                                    <comment>Above 'div' statement is equivalent to below 'Col'</comment>
+                                */}
+                                <Col md={10}>
+                                    {/*<comment>Tying component state to the state of this Input form element: value={this.state.firstname}</comment>*/}
+                                    <Input type="text" id="firstname" name="firstname" placeholder="First Name"
+                                    value={this.state.firstname} onChange={this.handleInputChange}/>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="lastname" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="lastname" name="lastname" placeholder="Last Name"
+                                    value={this.state.lastname} onChange={this.handleInputChange}/>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="telnum" md={2}>Telephone No.</Label>
+                                <Col md={10}>
+                                    <Input type="tel" id="telnum" name="telnum" placeholder="Telephone No."
+                                    value={this.state.telnum} onChange={this.handleInputChange}/>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Input type="email" id="email" name="email" placeholder="Email"
+                                    value={this.state.email} onChange={this.handleInputChange}/>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                {/*<comment>
+                                    Plugging in JS code in JSX 'Col' element
+                                    the JS object specifies the layout in terms of size of field/column & no. of columns to offset
+                                </comment>*/}
+                                <Col md={{size:6, offset:2}}>
+                                    <FormGroup check>
+                                        <Label check>
+                                            {/*<comment>
+                                                1. For input type - checkbox, 'checked' attribute TIES the component and form states
+                                                2. {' '} - to add space to the view
+                                            </comment>*/}
+                                            <Input type="checkbox" name="agree"
+                                            checked={this.state.agree} onChange={this.handleInputChange}/>{' '}
+                                            <strong>May we contact you?</strong>
+                                        </Label>
+                                    </FormGroup>
+                                </Col>
+                                <Col md={{size:3, offset:1}}>
+                                    <Input type="select" name="contactType"
+                                    value={this.state.contactType} onChange={this.handleInputChange}>
+                                        <option>Tel.</option>
+                                        <option>Email</option>
+                                    </Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="message" md={2}>Your Feedback</Label>
+                                <Col md={10}>
+                                    <Input type="textarea" id="message" name="message" rows="12"
+                                    value={this.state.message} onChange={this.handleInputChange}/>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size:10, offset:2}}>
+                                    <Button type="submit" color="primary">Send Feedback</Button>
+                                </Col>
+                            </FormGroup>
+                        </Form>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Contact;
